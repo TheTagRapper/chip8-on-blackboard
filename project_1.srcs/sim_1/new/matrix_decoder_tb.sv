3231a9f8afc6927cdffe80a43f2a8454d78dc581
TheTagRapper <pskkjk201@outlook.com>
2026-07-01 19:08:50 +0100

    Decoder now mostly works. Need to fix bug on last row + actually make it so it reads from the columns when it parses

--- project_1.srcs/sim_1/new/matrix_decoder_tb.sv
+++ project_1.srcs/sim_1/new/matrix_decoder_tb.sv
@@ -22,43 +22,24 @@
 
 module matrix_decoder_tb;
 
-initial #200ns $finish;
-
-logic r1, r2, r3, r4, c1, c2, c3, c4;
+logic JC1, JC2, JC3, JC4; // JC1 is row 0 
+logic JC7, JC8, JC9, JC10; // JC7 is col 0;
 logic clk;
+logic [3:0] sel_number;
+
+matrix_decoder md0 (.*);
 
 always #2ns clk <= ~clk;
 
-logic [3:0] end_number;
-
-matrix_decoder md0 ( .JC1(r1), .JC2(r2), .JC3(r3), .JC4(r4),
-         .JC7(c1), .JC8(c2), .JC9(c3), .JC10(c4), 
-         .clk(clk), 
-         .sel_number(end_number)
-         );
-
-//string status;
-
 initial begin
-
+    
+    // Ensuring 0 is yielded
+    JC7 = 0; JC8 = 0; JC9 = 0; JC10 = 0;
+        
     clk = 0;
-    //c1 = 0;
-    c2 = 0;
-    c3 = 0;
-    c4 = 0;
-    //status = "IDLE";
     
-    
-    // Testing Basic Input
-    c1 = 1; //status = "TESTING BASIC INPUT";
-    
-    #8ns c1 = 0; c2 = 1; 
-    
-    #16ns c2 = 0; c3 = 1;
-    
-    #24ns c3 = 0; c4 = 1;
-     
-    // Testing multiple columns pressed
+    #64ns JC7 = 1;
+
 end
 
 endmodule
--- project_1.srcs/sources_1/new/matrix_decoder.sv
+++ project_1.srcs/sources_1/new/matrix_decoder.sv
@@ -24,49 +24,52 @@ module matrix_decoder(
         output logic JC1, JC2, JC3, JC4, 
         input logic JC7, JC8, JC9, JC10,
         input logic clk,
-        output logic [3:0] sel_number
+  		output logic [3:0] sel_number,
+  		output logic [1:0] row_no,
+  		output logic [1:0] col_no,
+  		input logic nReset
     );
     
-    logic [3:0] row_drive, column_drive;
-    
-    assign {JC4, JC3, JC2, JC1} = row_drive;
-    assign column_drive = {JC10, JC9, JC8, JC7};
-    
-    logic [1:0] row_no;
-    
-    logic [1:0] col_no;
+   
+   // Read from columns, write to rows
     
     
-    // Setting the col no so they can scan
-    always_ff @(posedge clk) begin     
-         if (col_no == 2'b00) row_no += 1;
-         col_no += 1;
-    end
+    //logic [1:0] row_no;
+    //logic [1:0] col_no;
+    
+  		always_ff @(posedge clk or negedge nReset) begin
        
-       
-    // Setting the row_number
+          
+               // Setting iteration
+            if (nReset) begin  
+              if (col_no == 2'b11) row_no <= row_no + 1;
+              col_no <= col_no + 1;
+            end
+            else begin
+              row_no <= 2'b00;
+              col_no <= 2'b00;
+            end
+             
+        end
+    
+    // Setting which row to write
     always_comb begin
         case (row_no)
-            2'b00 : row_drive = 4'b0001;
-            2'b01 : row_drive = 4'b0010;
-            2'b10 : row_drive = 4'b0100;
-            2'b11 : row_drive = 4'b1000;
+            2'b00 : {JC1, JC2, JC3, JC4} = 4'b1000;
+            2'b01 : {JC1, JC2, JC3, JC4} = 4'b0100;
+            2'b10 : {JC1, JC2, JC3, JC4} = 4'b0010;
+            2'b11 : {JC1, JC2, JC3, JC4} = 4'b0001;
         endcase
+     end
+     
+  // Parsing out to sel_number (TO CHANGE SO IT DEPENDS FROM COLUMN INPUT)]
+  always_comb begin
+          if ((col_no != 2'b11) & (row_no != 2'b11)) sel_number <= ({2'b00, row_no} << 1) + (1 + col_no); // Digits 
+        else if (col_no == 2'b11) sel_number <= {2'b00, row_no} + 4'hC; // Last Column Hex Digits
+          else if (col_no == 2'bx0) sel_number <= ({2'b00 , col_no} << 1) + 4'hA; // A and B
+        else sel_number <= 4'b0000; // 0
+  end
         
-    end
-    
-    //sel_number <= (((row_no_comp[7:4] - 1) >> 1) + 1 + row_no_comp[3:0]);
-    
-    logic [7:0] row_no_comp;
-    assign row_no_comp = { ((row_drive - 1) << 2), ((column_drive -1) << 2)};
-   
-   always_ff  @(posedge clk) begin
-        if (row_no_comp != 8'hCC) begin // Catch Exception
-            if ( row_no_comp[3:0] != 4'h4) sel_number <= 4'hB + row_no_comp[7:4];
-            else if ( row_no_comp[7:4] != 4'h4 ) sel_number <= (((row_no_comp[7:4] - 1) >> 1) + 1 + row_no_comp[3:0] );
-            else if ( row_no_comp[4] == 1'b1) sel_number <= 4'h9 + ( row_no_comp[3:0] >> 1 ) + 1;
-            else sel_number <= 4'h0;
-        end
-   end
         
+     
 endmodule
